c88acdd3f584d4d6aa1d7e8d050bc5de7d3bc124
Walter C <incruz.wjcr@gmail.com>
2023-11-25 15:25:07 -0500

    ingresando cambios

---
+++ blogs/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    overview = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+    categories = models.ManyToManyField('Categorias')
+    featured = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+    
+
+class Categorias(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name_plural = 'Categorias'
+        
+    
+    
---
+++ blogs/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+from blogs.models import Post, Categorias
+
+# Create your views here.
+
+def home_page(request):
+    posts = Post.objects.all()
+    categorias = Categorias.objects.all()
+    featured = Post.objects.filter(featured= True)[:3]
+
+    context = { 
+        'posts':posts,
+        'categorias':categorias,
+        'destacados': featured
+        
+    }
+
+    return render(request, 'blogs/home_page.html', context=context)
+
