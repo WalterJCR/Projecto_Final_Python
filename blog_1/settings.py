4733d8d72ec8054fb2a7205850ef9010ff9a622c
Walter C <incruz.wjcr@gmail.com>
2023-11-28 20:44:58 -0500

    Se agrego login (Sign In /Sign Out)

--- blog_1/settings.py
+++ blog_1/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'blog_1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -128,3 +128,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'usuarios.User'
+
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
--- blog_1/urls.py
+++ blog_1/urls.py
@@ -21,5 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('account/', include ('django.contrib.auth.urls')),
     path('', include('blogs.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
